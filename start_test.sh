f83c8aa73431c6889c32ad200f39d5afafada111
Devasia Manuel <devasia1000@gmail.com>
2013-06-25 18:57:03 -0400

    Fixed counter bug

--- start_test.sh
+++ start_test.sh
@@ -3,7 +3,7 @@
 #These variables are user-editable
 bandwidth=350 #in kilobits per second
 iterations=100
-videoPlayTime=300 #in seconds
+videoPlayTime=5 #in seconds
 
 #These variables are NOT user-editable
 i=0
@@ -19,17 +19,14 @@ while [ $i -lt $iterations ]; do
 	sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*
 	
 	#Start bandwidth throttling and start Chromium
-	trickle -d $bandwidth -u 10000 ./out/Release/chrome http://127.0.0.1/start.html
+	trickle -d $bandwidth -u 10000 ./out/Release/chrome http://127.0.0.1/start.html | tee /home/devasia/Desktop/src/log/chromium${i}.txt &
 
 	#Wait for video to play a while
 	sleep $videoPlayTime
 	#Kill chromium
 	sudo killall chrome
 
-	#Kill bandwidth throttling process
-	sudo killall trickle
-	sudo killall trickled
-
 	#Increment counter 
-	let i=i+1
+	i=$((i+1))
+	echo $i
 done
