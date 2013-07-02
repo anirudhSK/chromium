bbbfe481a35e684b00fb447e534ba8200008e953
Devasia Manuel <devasia1000@gmail.com>
2013-07-02 10:17:56 -0400

    Modified startup script to change bandwidth while playing videoWq

--- start_test.sh
+++ start_test.sh
@@ -3,7 +3,7 @@
 #These variables are user-editable
 
 iterations=100
-videoPlayTime=6000 #in seconds
+videoPlayTime=20 #in seconds
 
 #These variables are NOT user-editable
 i=0
@@ -18,17 +18,20 @@ while [ $i -lt $iterations ]; do
 	sudo rm -rfv ~/.cache/google-chrome/Default/Media\ Cache/*
 	sudo rm -rfv ~/.cache/google-chrome/Default/Cache/*
 	
-	#Start bandwidth throttling and start Chromium
-~/Desktop/src/out/Release/chrome http://10.0.0.2/start_sita.html | perl /home/devasia/Desktop/src/process.pl
+	#Start bandwidth throttling
+	sudo ipfw pipe 2 config bw 8Mbit/s
+
+	#Start Chromium
+	~/Desktop/src/out/Release/chrome
 
 	#Wait for video to play a while
-	sleep $videoPlayTime
+        sleep $videoPlayTime
+	
+	#Change bandwidth throttling
+	sudo ipfw pipe 2 config bw 1Mbit/s
+	
 	#Kill chromium
 	sudo killall chrome
-
-	#Kill bandwidth throttling process
-	sudo killall trickle
-	sudo killall trickled
 
 	#Increment counter 
 	let i=i+1;
